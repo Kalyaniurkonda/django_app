258c8449941c7824e8ef09a588a1e97d4210f43e
kalyaniurkonda <kalyaniurkonda@gmail.com>
2024-02-22 19:08:57 +0530

    creating url for an application

--- schoolapp/admissions/views.py
+++ schoolapp/admissions/views.py
@@ -4,7 +4,6 @@ from django.http import HttpResponse
 # Create your views here.
 def admissions1(request):
     return HttpResponse("this is new admissions page")
-
 def admissions2(request):
     return HttpResponse("this is  old admissions  page")
 
