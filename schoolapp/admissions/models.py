7f4746ad5def333c195e3c63b0425b4495375756
kalyaniurkonda <kalyaniurkonda@gmail.com>
2024-03-13 18:00:22 +0530

    models -ORM - One

--- schoolapp/admissions/models.py
+++ schoolapp/admissions/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class student(models.Model):#create table table_name
+    name = models.CharField(max_length=100)
+    fathername = models.CharField(max_length=200)
+    age = models.IntegerField()
+    mobole = models.CharField(max_length=100)
--- schoolapp/admissions/views.py
+++ schoolapp/admissions/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 # from django.http import HttpResponse
+from admissions.models import student
 
 # Create your views here.
 def homepage(request):
@@ -10,8 +11,10 @@ def admissions1(request):
     return render(request,'admissions/newadmissions.html',values)
 
 def admissions2(request):
-    values = {'Name':'Kalyani','Age':23,'Adress':'hyd'}
-    return render(request,'admissions/oldadmissions.html',values)
+    # values = {'Name':'Kalyani','Age':23,'Adress':'hyd'}
+    result = student.objects.all() #select*from student
+    students={'allstudents':result}
+    return render(request,'admissions/oldadmissions.html',students)
  
 
 
