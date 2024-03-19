dc5f439e990191b5f7a3d56446f5689b2d729963
kalyaniurkonda <kalyaniurkonda@gmail.com>
2024-03-19 11:53:42 +0530

    inserting data into tables in sqlite and mysql and performing actions in admin.py

--- schoolapp/admissions/admin.py
+++ schoolapp/admissions/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from admissions.models import student
 
 # Register your models here.
+class studentAdmin(admin.ModelAdmin):
+    list_display=['id']
+
+
+admin.site.register(student)
--- schoolapp/schoolapp/settings.py
+++ schoolapp/schoolapp/settings.py
@@ -77,8 +77,10 @@ WSGI_APPLICATION = 'schoolapp.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'schoolapp',
+        'USER' : 'schoolappuser',
+        'PASSWORD' : 'Kalyani@mysql1',
     }
 }
 
